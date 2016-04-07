ab9381a3c235f6619ae9b63563e6d3addbd34562
Steve Worley <steven.worley@acquia.com>
2016-04-07 13:31:24 +1000

    Fixes issues with entity_uri() causing errors
    Beans uses entity_class_uri as the entity_uri callback, as we were building the entity with a standard object the object does not have the URI method and this would cause the fatal errors.

--- src/Uri.php
+++ src/Uri.php
@@ -11,6 +11,9 @@ class Uri {
 
   const LOAD_HOOK = '%content_hub_extend';
   const ENTITY_ID = 1;
+  const ENTITY_NAME = 'temp_entity';
+
+  private $data = array();
 
   /**
    * Uri constructor.
@@ -29,7 +32,42 @@ class Uri {
   }
 
   public static function generatePath($path) {
-    return str_replace(self::ENTITY_ID, self::LOAD_HOOK . "/ch", $path);
+    return str_replace(self::searchers(), self::LOAD_HOOK . "/ch", $path);
+  }
+
+  /**
+   * Words that can be present in the entity uri.
+   *
+   * @return array
+   */
+  public static function searchers() {
+    return array(self::ENTITY_ID, self::ENTITY_NAME);
+  }
+
+  /**
+   * Dot notation accessor.
+   *
+   * @param null $path
+   * @param null $default
+   *
+   * @return mixed
+   *   A point in $this->data.
+   */
+  public function get($path = NULL, $default = NULL) {
+    $array = $this->data;
+
+    if (empty($path)) {
+      return $default;
+    }
+
+    $keys = explode('.', $path);
+    foreach ($keys as $key) {
+      if (isset($array[$key])) {
+        $array = $array[$key];
+      }
+    }
+
+    return $array == $this->data ? $default : $array;
   }
 
   /**
@@ -43,13 +81,13 @@ class Uri {
     $key = 0;
 
     foreach ($this->parts as $index => $part) {
-      if ($part == self::ENTITY_ID) {
+      if (in_array($part, self::searchers())) {
         $key = $index;
         break;
       }
     }
 
-    return array(
+    $this->data = array(
       'path' => static::generatePath($this->path),
       'args' => array(
         'page' => array($this->type, $key),
@@ -57,5 +95,7 @@ class Uri {
         'load' => array($this->type, $key),
       ),
     );
+
+    return $this;
   }
 }
